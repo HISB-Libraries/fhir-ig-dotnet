6d3b2728867b8dfdcec05d057537fa026408bbf3
Brian Ritchie <britchie3@gatech.edu>
2021-11-23 20:51:55 -0500

    Initial add of CbsLabObservationProfile

---
+++ src/GaTech.Chai.Cbs/CbsLabObservationProfile/CbsLabObservation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Hl7.Fhir.Model;
+using GaTech.Chai.Cbs.Extensions;
+
+namespace GaTech.Chai.Cbs.CbsLabObservationProfile
+{
+    /// <summary>
+    /// Case Based Surveillance Lab Observation Profile Extensions
+    /// http://cbsig.chai.gatech.edu/StructureDefinition/cbs-lab-observation
+    /// </summary>
+    public class CbsLabObservation
+    {
+        readonly Observation observation;
+
+        internal CbsLabObservation(Observation o)
+        {
+            this.observation = o;
+        }
+
+        /// <summary>
+        /// Factory for Case Based Surveillance Lab Observation Profile
+        /// http://cbsig.chai.gatech.edu/StructureDefinition/cbs-lab-observation
+        /// </summary>
+        public static Observation Create()
+        {
+            var observation = new Observation();
+            observation.CbsLabObservation().AddProfile();
+            observation.Category.Add(new CodeableConcept("http://terminology.hl7.org/CodeSystem/observation-category", "laboratory"));
+            observation.Status = ObservationStatus.Final;
+            return observation;
+        }
+
+        /// <summary>
+        /// The official URL for the Case Based Surveillance Lab Observation profile, used to assert conformance.
+        /// </summary>
+        public const string ProfileUrl = "http://cbsig.chai.gatech.edu/StructureDefinition/cbs-lab-observation";
+
+        /// <summary>
+        /// Set the assertion that an observation object conforms to the Case Based Surveillance Lab Observation profile.
+        /// </summary>
+        public void AddProfile()
+        {
+            this.observation.AddProfile(ProfileUrl);
+        }
+
+        /// <summary>
+        /// Clear the assertion that an observation object conforms to the Case Based Surveillance Lab Observation profile.
+        /// </summary>
+        public void RemoveProfile()
+        {
+            this.observation.RemoveProfile(ProfileUrl);
+        }
+    }
+}
---
+++ src/GaTech.Chai.Cbs/CbsLabObservationProfile/CbsLabObservationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Hl7.Fhir.Model;
+using GaTech.Chai.Cbs.Extensions;
+
+namespace GaTech.Chai.Cbs.CbsLabObservationProfile
+{
+    /// <summary>
+    /// Class with Observation extensions for Case Based Surveillance Lab Observation profile.
+    /// http://cbsig.chai.gatech.edu/StructureDefinition/cbs-lab-observation
+    /// </summary>
+    public static class CbsLabObservationExtensions
+    {
+        public static CbsLabObservation CbsLabObservation(this Observation observation)
+        {
+            return new CbsLabObservation(observation);
+        }
+    }
+
+}
