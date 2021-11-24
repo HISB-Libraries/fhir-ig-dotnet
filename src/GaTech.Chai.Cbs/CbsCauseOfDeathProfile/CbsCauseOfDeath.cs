06d9a244d8782c6729da079901d5eb6fc73a0f74
Brian Ritchie <britchie3@gatech.edu>
2021-11-23 20:50:57 -0500

    Initial add of CbsCauseOfDeath

---
+++ src/GaTech.Chai.Cbs/CbsCauseOfDeathProfile/CbsCauseOfDeath.cs
@@ -0,0 +1,52 @@
+﻿using Hl7.Fhir.Model;
+using GaTech.Chai.Cbs.Extensions;
+
+namespace GaTech.Chai.Cbs.CbsCaseOfDeathProfile
+{
+    /// <summary>
+    /// Case Based Surveillance Lab Cause of Death Profile Extensions
+    /// http://cbsig.chai.gatech.edu/StructureDefinition/cbs-cause-of-death
+    /// </summary>
+    public class CbsCaseOfDeath
+    {
+        readonly Observation observation;
+
+        internal CbsCaseOfDeath(Observation o)
+        {
+            this.observation = o;
+        }
+
+        /// <summary>
+        /// Factory for Case Based Surveillance Cause of Death Profile
+        /// http://cbsig.chai.gatech.edu/StructureDefinition/cbs-cause-of-death
+        /// </summary>
+        public static Observation Create()
+        {
+            var observation = new Observation();
+            observation.CbsCaseOfDeath().AddProfile();
+            observation.Category.Add(new CodeableConcept("http://loinc.org", "79378-6"));
+            observation.Status = ObservationStatus.Final;
+            return observation;
+        }
+        /// <summary>
+        /// The official URL for the Case Based Surveillance Cause of Death profile, used to assert conformance.
+        /// </summary>
+        public const string ProfileUrl = "http://cbsig.chai.gatech.edu/StructureDefinition/cbs-cause-of-death";
+
+        /// <summary>
+        /// Set the assertion that an observation object conforms to the Cause of Death Profile.
+        /// </summary>
+        public void AddProfile()
+        {
+            this.observation.AddProfile(ProfileUrl);
+        }
+
+        /// <summary>
+        /// Clear the assertion that an observation object conforms to the Cause of Death Profile.
+        /// </summary>
+        public void RemoveProfile()
+        {
+            this.observation.RemoveProfile(ProfileUrl);
+        }
+    }
+}
---
+++ src/GaTech.Chai.Cbs/CbsCauseOfDeathProfile/CbsCauseOfDeathExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Hl7.Fhir.Model;
+
+namespace GaTech.Chai.Cbs.CbsCaseOfDeathProfile
+{
+    /// <summary>
+    /// Class with Observation extensions for Case Based Surveillance Cause of Death Profile
+    /// http://cbsig.chai.gatech.edu/StructureDefinition/cbs-cause-of-death
+    /// </summary>
+    public static class CbsCaseOfDeathExtensions
+    {
+        public static CbsCaseOfDeath CbsCaseOfDeath(this Observation observation)
+        {
+            return new CbsCaseOfDeath(observation);
+        }
+    }
+}
