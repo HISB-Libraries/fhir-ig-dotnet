55cffe1a21252be998b3a581b26f5b32e692b7d6
Brian Ritchie <britchie3@gatech.edu>
2021-11-30 22:04:43 -0500

    Initial add of CbsVaccinationRecordProfile

---
+++ src/GaTech.Chai.Cbs/CbsVaccinationRecordProfile/CbsVaccinationRecord.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Hl7.Fhir.Model;
+using GaTech.Chai.Cbs.Extensions;
+
+namespace GaTech.Chai.Cbs.CbsVaccinationRecordProfile
+{
+    /// <summary>
+    /// Case Based Surveillance Vaccinaton Record Profile Extensions
+    /// http://cbsig.chai.gatech.edu/StructureDefinition/cbs-vaccination-record
+    /// </summary>
+    public class CbsVaccinationRecord
+    {
+        readonly Immunization immunization;
+
+        internal CbsVaccinationRecord(Immunization immunization)
+        {
+            this.immunization = immunization;
+        }
+
+        /// <summary>
+        /// Factory for Case Based Surveillance Vaccinaton Record Profile
+        /// http://cbsig.chai.gatech.edu/StructureDefinition/cbs-vaccination-record
+        /// </summary>
+        public static Immunization Create()
+        {
+            var immunization = new Immunization();
+            immunization.CbsVaccinationRecord().AddProfile();
+            return immunization;
+        }
+
+        /// <summary>
+        /// The official URL for the Case Based Surveillance Vaccinaton Record profile, used to assert conformance.
+        /// </summary>
+        public const string ProfileUrl = "http://cbsig.chai.gatech.edu/StructureDefinition/cbs-vaccination-record";
+
+        /// <summary>
+        /// Set the assertion that a immunization object conforms to the Case Based Surveillance Vaccinaton Record Profile.
+        /// </summary>
+        public void AddProfile()
+        {
+            immunization.AddProfile(ProfileUrl);
+        }
+
+        /// <summary>
+        /// Clear the assertion that a immunization object conforms to the Case Based Surveillance Vaccinaton Record Profile.
+        /// </summary>
+        public void RemoveProfile()
+        {
+            immunization.RemoveProfile(ProfileUrl);
+        }
+    }
+}
---
+++ src/GaTech.Chai.Cbs/CbsVaccinationRecordProfile/CbsVaccinationRecordExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Hl7.Fhir.Model;
+
+namespace GaTech.Chai.Cbs.CbsVaccinationRecordProfile
+{
+    /// <summary>
+    /// Class with Immunization extensions for Case Based Surveillance Vaccinaton Record Profile
+    /// http://cbsig.chai.gatech.edu/StructureDefinition/cbs-vaccination-record
+    /// </summary>
+    public static class CbsVaccinationRecordExtension
+    {
+        public static CbsVaccinationRecord CbsVaccinationRecord(this Immunization immunization)
+        {
+            return new CbsVaccinationRecord(immunization);
+        }
+    }
+}
